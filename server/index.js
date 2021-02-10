7df6c81ce7920556a67ac7f22106d8334cac4dff
aditya <aditya7238195@gmail.com>
2021-02-10 20:36:28 +0530

    changes on backend updated

--- server/index.js
+++ server/index.js
@@ -18,38 +18,21 @@ app.use(router);
 
 io.on('connection',(socket) => {
     
-    // socket.on('join', ({name, room}, callback) =>{
-    //     const {error, user} = addUser({id:socket.id, name, room});
+    socket.on('join', ({name, room, uniCode}, callback) =>{
+        const {error, user} = addUser({id:socket.id, name, room, uniCode});
 
-    //     if(error) return callback(error);
+        if(error) return callback(error);
 
-    //     socket.join(user.room);
-    //     io.to(user.room).emit('test',{data:"this is me "})
-    //     socket.broadcast.to(user.room).emit('message', {user: 'admin', text :`${user.name} hopped into the ${user.room}`, type: 'text' });
-    //     // io.to(user.room).emit('ActiveUsers', {users: getUsersInRoom(user.room) });
-    //      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-    //     // console.log(getUsersInRoom(user.room));
-    //     callback();
-    // });
+        socket.join(user.room);
+        socket.emit('message',{user:'admin',text:`Welcome to the room ${user.room}`})
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text :`${user.name} hopped into the ${user.room}`});
+         io.to(user.room).emit('ActiveUsers', { users: getUsersInRoom(user.room) });
+        callback();
+    });
 
-    socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
-
-    socket.join(user.room);
-
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
-    callback();
-  });
-
-    socket.on('sendMessage', () =>{
+    socket.on('sendMessage', (message, callback) => {
         const user= getUser(socket.id);
-
         io.to(user.room).emit('message',{user:user.name, text: message});
 
         callback();
@@ -60,10 +43,10 @@ io.on('connection',(socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
         if(user){
-            io.to(user.room).emit('message', {user: 'admin', text:`${user.name} just left the room`, type:"text"});
-            
+            io.to(user.room).emit('message', {user: 'admin', text:`${user.name} just left the room`});
+            io.to(user.room).emit('ActiveUsers', { users: getUsersInRoom(user.room) });
+
         }
-        console.log("user had left");
     })
 })
 app.use(router);
--- server/users.js
+++ server/users.js
@@ -1,15 +1,15 @@
 const users = [];
 
-const addUser = ({ id, name, room }) => {
+const addUser = ({ id, name, room,uniCode }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
-  const existingUser = users.find((user) => user.room === room && user.name === name);
+  // const existingUser = users.find((user) => user.room === room && user.name === name);
 
-  if(!name || !room) return { error: 'Username and room are required.' };
-  if(existingUser) return { error: 'Username is taken.' };
+  // if(!name || !room) return { error: 'Username and room are required.' };
+  // if(existingUser) return { error: 'Username is taken.' };
 
-  const user = { id, name, room };
+  const user = { id, name, room,uniCode };
 
   users.push(user);
 
